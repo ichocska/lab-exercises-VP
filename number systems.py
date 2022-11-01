4c324607507afae92c6c897411e9affbc9d0093b
ichocska <ichocska12@gmail.com>
2022-11-01 18:24:53 +0200

    Deleted unnecesary files

--- number systems.py
+++
@@ -1,8 +0,0 @@
-number = 121222193
-date = 20102022
-
-print(f"\nExercise 1\n{number}")
-print(f"Decimal to Binary: {bin(number)}\nBinary to Octal: {oct(number)}\nBinary to Hexadecimal: {hex(number)}")
-
-print(f"\nExercise 2\n{date}")
-print(f"Decimal to Binary: {bin(date)}\nBinary to Octal: {oct(date)}\nBinary to Hexadecimal: {hex(date)}")
